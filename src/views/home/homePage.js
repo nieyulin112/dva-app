f0e0e8c00e88e2b7c5a0e73a56c893affe0ba71b
nieyulin112 <1139723906@qq.com>
2020-05-09 09:44:50 +0800

    feat: antd-mobile的注入

--- src/views/home/homePage.js
+++ src/views/home/homePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
+import { Button, Modal } from 'antd-mobile';
 // import PropTypes from 'prop-types';
 class HomePage extends React.Component {
     componentDidMount() {
@@ -16,21 +17,25 @@ class HomePage extends React.Component {
                     <li onClick={this.skipIndex}>333</li>
                 </ul>
                 <div onClick={this.test}>测试</div>
+                <Button type="warning" onClick={this.btnMsg}>warning</Button>
             </div>
         )
     }
-  skipIndex = () => {
-      this.props.dispatch(routerRedux.push('/index'))
-  }
-  test = () => {
-      let s = {
-          a: 1
-      }
-      this.props.dispatch({
-          type: 'homePage/fetch',
-          payload: { ...s }
-      })
-  }
+    skipIndex = () => {
+        this.props.dispatch(routerRedux.push('/index'))
+    }
+    test = () => {
+        let s = {
+            a: 1
+        }
+        this.props.dispatch({
+            type: 'homePage/fetch',
+            payload: { ...s }
+        })
+    }
+    btnMsg = () => {
+        Modal.alert('消息', '侬好');
+    }
 }
 HomePage.propTypes = {
 };
