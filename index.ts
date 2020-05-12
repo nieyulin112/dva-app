4b96df5e5203a571d8a8183c4e7285b7a7eabdb3
nieyulin112 <1139723906@qq.com>
2020-05-12 17:25:37 +0800

    index

--- index.ts
+++ index.ts
@@ -6,11 +6,11 @@ let num : number = 12;
 
 let str : string = '222';
 
-let arr : number[] = [1, 2, 3]
+let arr : number[] = [1, 2, 3];
 
-let list: Array<number> = [1,2,3,4] // 使用范型调用
+let list: Array<number> = [1,2,3,4]; // 使用范型调用
 
-let x : [string, number] = ['', 12] // 元组的定义
+let x : [string, number] = ['', 12]; // 元组的定义
 
 
 enum Color { Red, Green, Blue}
